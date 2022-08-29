f629731e8868f95f2891d1666cb124c837979df1
admin <zhanchengkun@126.com>
2022-08-29 16:35:02 +0800

    输出history?.location

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,5 +1,9 @@
 import { history } from "umi";
 export default function HomePage() { 
   console.log(history?.location);
-  return <div>test</div>;
+  return <div>test.js<br/>
+    history.location:
+    {
+    JSON.stringify(history?.location)
+    }</div>;
 }
--- src/pages/test2.js
+++ src/pages/test2.js
@@ -1,5 +1,9 @@
 import { history } from "umi";
 export default function HomePage() {
   console.log(history?.location);
-  return <div>test2</div>;
+  return <div>test2.js<br/>
+  history.location:
+    {
+ JSON.stringify(history?.location)
+    }</div>;
 }
